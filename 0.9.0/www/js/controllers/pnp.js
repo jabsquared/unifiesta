6f714cf16f5bace1cde0bff36817e2ea9815aa5e
Louis Vichy <lgvichy@gmail.com>
2015-08-26 14:31:31 -0700

    Added MapData Service, testing on the parking and parade map

--- 0.9.0/www/js/controllers/pnp.js
+++ 0.9.0/www/js/controllers/pnp.js
@@ -1,44 +1,127 @@
-app.controller('PnPCtrl', function($scope, $state, $stateParams, $ionicHistory) {
+app.controller('PnPCtrl', function($scope, $state, $stateParams, leafletData, $ionicHistory, mapData) {
 
-  $scope.goBack = function() {
-    $ionicHistory.goBack();
+  $scope.findMe = false;
+  $scope.lat = 0;
+  $scope.long = 0;
+  $scope.markers = mapData.pnp;
+  $scope.iconColor = {
+    color: '#DDDDDDFF'
   };
 
+  $scope.info = {};
+  $scope.showCard = false;
+
+  var watchID;
+  // Fetching frequency, every sec...
+  var watchOptions = {
+    frequency: 1000,
+    timeout: 3000,
+    enableHighAccuracy: false // may cause errors if true
+  };
+
+  $scope.dissableGeoLocation = function() {
+    $scope.findMe = false;
+    console.log('Clearing watchID');
+    $scope.iconColor = {
+      color: '#DDDDDDFF'
+    };
+
+    navigator.geolocation.clearWatch(watchID);
+
+    if ($scope.markers.length > 3) {
+      $scope.markers.pop();
+    }
+    console.log($scope.markers);
+    $scope.auburn.lat = 47.307492;
+    $scope.auburn.lng = -122.230582;
+  };
+
+  $scope.toggleGeoLocation = function() {
+    $scope.findMe = !$scope.findMe;
+    if ($scope.findMe) {
+      console.log('About to get location');
+      $scope.iconColor = {
+        color: '#387EF5'
+      };
+      watchID = navigator.geolocation.watchPosition(
+        onSuccess,
+        onError, {
+          enableHighAccuracy: false
+        });
+      return;
+    }
+    $scope.dissableGeoLocation();
+  };
+
+  var onSuccess = function(position) {
+    console.log('rending location!');
+    $scope.lat = position.coords.latitude;
+    $scope.long = position.coords.longitude;
+    console.log('marker lat: ' + $scope.lat);
+    console.log('marker long: ' + $scope.long);
+
+    // Since Watching fetch data many time, this call will just stack up marker over and over again...
+
+    if ($scope.markers.length > 3) {
+      $scope.markers.pop();
+    }
+
+    $scope.markers.push({
+      lat: position.coords.latitude,
+      lng: position.coords.longitude,
+      focus: true,
+      // TODO: Fix auto focus. Only works with message.
+      message: 'You are Here!',
+      draggable: false,
+      icon: {
+        iconUrl: '/img/location.png'
+      }
+    });
+    $scope.auburn.lat = $scope.lat;
+    $scope.auburn.lng = $scope.long;
+  };
+
+  // onError Callback receives a PositionError object
+  var onError = function(error) {
+    alert('code: ' + error.code + '\n' +
+      'message: ' + error.message + '\n');
+  };
+
+
+  // Map Data:
+
   angular.extend($scope, {
-    tiles: {
-      url: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-    },
+    tiles: mapData.tiles,
     auburn: {
       lat: 47.307492,
       lng: -122.230582,
       zoom: 17,
       bounceAtZoomLimits: true
     },
-    markers: {
-      m1: {
-        lat: 47.307492,
-        lng: -122.230582,
-        focus: true,
-        draggable: false,
-        message: "Hi there!",
-        icon: {}
-      }
-    },
     events: {
       markers: {
-        enable: ['dragend']
+        enable: ['click']
           //logic: 'emit'
       }
+    },
+    controls: {
+      scale: false
+    },
+  });
+
+  $scope.$on('leafletDirectiveMarker.click', function(e, args) {
+    if (args.leafletEvent.target.options.info) {
+      console.log(args.leafletEvent.target.options.info);
+      $scope.info = args.leafletEvent.target.options.info;
+      $scope.showCard = true;
+    } else {
+      $scope.info = {};
+      $scope.showCard = false;
     }
   });
 
-  $scope.$on("leafletDirectiveMarker.dragend", function(event, args) {
-    console.log('hola');
-    $scope.markers.m1.lat = args.model.lat;
-    $scope.markers.m1.lng = args.model.lng;
-  });
-
-  $scope.goToSchedule = function() {
-    $state.go('schedule');
+  $scope.goBack = function() {
+    $scope.dissableGeoLocation();
+    $ionicHistory.goBack();
   };
 });
