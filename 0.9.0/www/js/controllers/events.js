95b0dd1ba9511509a6c06cc083d20fef50657918
bpshonyak <bpshonyak@live.com>
2015-08-31 19:21:50 -0700

    added id to cards and shit

--- 0.9.0/www/js/controllers/events.js
+++ 0.9.0/www/js/controllers/events.js
@@ -1,4 +1,4 @@
-app.controller('EventsCtrl', function($scope, $rootScope, $state, reminderService, $ionicPlatform, eventData, $ionicHistory) {
+app.controller('EventsCtrl', function($scope, $ionicScrollDelegate, $rootScope, $state, reminderService, $ionicPlatform, eventData, $ionicHistory) {
 
   $scope.goBack = function() {
     $rootScope.showFooter = true;
@@ -13,8 +13,11 @@ app.controller('EventsCtrl', function($scope, $rootScope, $state, reminderServic
     $state.go(path);
   };
 
-  $scope.schedule = function (single_event) {
-    if (!single_event.reminder){
+  $scope.schedule = function(single_event) {
+
+    $ionicScrollDelegate.scrollBottom();
+
+    if (!single_event.reminder) {
       reminderService.schedule(single_event);
     } else {
       reminderService.cancel(single_event.id);
