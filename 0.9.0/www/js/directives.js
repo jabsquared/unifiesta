a2d6b378b38d2e32c37706306fc85f5449ebaefd
Louis Vichy <lgvichy@gmail.com>
2015-09-19 22:59:17 -0700

    Some kind of indentation

--- 0.9.0/www/js/directives.js
+++ 0.9.0/www/js/directives.js
@@ -5,47 +5,47 @@
 //   };
 // });
 // app.directive('imageonload ', function() {
-  //   return {
-  //     restrict: 'A',
-  //     link: function(scope, element, attrs) {
-  //       element.bind('load', function() {
-  //         //call the function that was passed
-  //         scope.$apply(attrs.imageonload);
-  //       });
-  //     }
-  //   };
-  // });
-app.directive('imageonload',  function() {
+//   return {
+//     restrict: 'A',
+//     link: function(scope, element, attrs) {
+//       element.bind('load', function() {
+//         //call the function that was passed
+//         scope.$apply(attrs.imageonload);
+//       });
+//     }
+//   };
+// });
+app.directive('imageonload', function() {
 
-    'use strict';
+  'use strict';
 
-    return {
-      restrict: 'A',
+  return {
+    restrict: 'A',
 
-      link: function(scope, element, attrs) {
+    link: function(scope, element, attrs) {
 
-        var cssClass = attrs.loadedclass,
-          origSrc,
-          timeout;
+      var cssClass = attrs.loadedclass;
+      var origSrc;
+      var timeout;
 
-        function reload() {
-          timeout = setTimeout(function() {
-            if (!origSrc) {
-              origSrc = element[0].src;
-            }
-            element[0].src = origSrc + '?' + Date.now();
-            reload();
-          }, 4500);
-        }
+      function reload() {
+        timeout = setTimeout(function() {
+          if (!origSrc) {
+            origSrc = element[0].src;
+          }
 
-        reload();
-
-        element.bind('load', function(e) {
-          clearTimeout(timeout);
-          element.addClass(cssClass);
-              scope.$apply(attrs.imageonload);
-        });
+          element[0].src = origSrc + '?' + Date.now();
+          reload();
+        }, 4500);
       }
-    };
-  }
-);
+
+      reload();
+
+      element.bind('load', function(e) {
+        clearTimeout(timeout);
+        element.addClass(cssClass);
+        scope.$apply(attrs.imageonload);
+      });
+    },
+  };
+});
